31131fce1e5d60e11ae34f9847316def1760689c
Tom Renn <tomrenn@google.com>
2025-01-29 16:11:59 -0800

    Add android_local_test target to show broken robolectric deps
    bazel test java/... --java_runtime_version=remotejdk_17
    
    Robolectric test fails with the error
    couldn't read ../rules_robolectric+/bazel/robolectric-deps.properties

---
+++ examples/basicapp/java/com/basicapp/BasicActivityTest.java
@@ -0,0 +1,15 @@
+package foo;
+
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import org.robolectric.RobolectricTestRunner;
+
+@RunWith(RobolectricTestRunner.class)
+public class BasicActivityTest {
+    @Test
+    public void clickingLogin_shouldStartLoginActivity() {
+    }
+}
